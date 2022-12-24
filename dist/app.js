a037d93c351921f32bb8d743adb2594cd9fb47c7
Ecaterina Pogorelova <kot91319@gmail.com>
2022-12-25 00:22:36 +0200

    working with DOMElements

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,19 @@ class ProjectInput {
         this.hostElement = document.getElementById('app');
         const importNode = document.importNode(this.templateElement.content, true);
         this.element = importNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.handleSubmit.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
